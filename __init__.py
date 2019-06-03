98aabbba4fb08073d51d4fde5f8763d1cd1cb522
hellricer <astrometry.bot@gmail.com>
2019-06-03 03:10:59 +0200

    fix indent

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ for module in os.listdir(os.path.dirname(__file__)):
     if module == '__init__.py' or module[-3:] != '.py':
         continue
     modules.append(__import__(module[:-3], locals(), globals()))
-del module
+    del module
 
 def _substitute(url, html):
     target = html
